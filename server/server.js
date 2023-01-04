cee310c103cd2c56dd146a72043862fe8d2034fb
Parimal Kashireddy <pkashi1@lsu.edu>
2023-01-04 10:22:19 -0600

    server is done working on client

--- server/server.js
+++ server/server.js
@@ -5,14 +5,19 @@ import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config()
 
+/* Creating a new configuration object with the API key. */
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
+/* Creating a new instance of the OpenAI API. */
 const openai = new OpenAIApi(configuration);
 
+/* Creating a new instance of the Express framework. */
 const app = express()
+/* It allows the frontend to make requests to the backend. */
 app.use(cors())
+/* It allows the frontend to make requests to the backend. */
 app.use(express.json())
 
 app.get('/', async (req, res) => {
@@ -23,11 +28,13 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
     try {
+        /* Getting the prompt from the frontend. */
         const prompt = req.body.prompt;
 
+        /* Calling the OpenAI API to generate a response. */
         const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
+            model: "text-davinci-003",//it can understand text as well as code
+            prompt: `${prompt}`,//passing the prompt from frontend
             temperature: 0, // Higher values means the model will take more risks.
             max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
             top_p: 1, // alternative to sampling with temperature, called nucleus sampling
@@ -37,6 +44,7 @@ app.post('/', async (req, res) => {
 
         /* Sending the response from the API to the frontend. */
         res.status(200).send({
+            /* The API returns an array of choices. We are just taking the first one. */
             bot: response.data.choices[0].text
         });
 
